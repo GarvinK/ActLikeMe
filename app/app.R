2e59ff7f6446ef7fa9f53518d1b794e11c64453c
GarvinK <tedstriker79@gmail.com>
2020-04-06 21:24:31 +0000

    reducing simulated days to 50

--- app/app.R
+++ app/app.R
@@ -103,14 +103,14 @@ server <- function(input, output, session) {
     #NLCommand(paste('set avg-relationships-per-person ',toString(input$avg_relationships_per_person),sep=""))
     
     #v <- NLDoReport(10, "go", "act-immune-people")
-    v <- NLDoReport(10, "go", c("act-immune-people","act-dead-people","act-infect-people","act-hosp-people","act-required-hosp"),
+    v <- NLDoReport(50, "go", c("act-immune-people","act-dead-people","act-infect-people","act-hosp-people","act-required-hosp"),
                     as.data.frame=TRUE, df.col.names=c("act_immune_people","act_dead_people","act_infect_people","act_hosp_people","act_required_hosp "))
     
     output$act_immune_people <- renderPlot({
       #if (is.null(v$data)) return()
       #plot(v$act_immune_people)
-      ggplot(v, aes(x=seq(1:100))) + 
-        coord_cartesian(xlim = c(0, 100), ylim = c(0, 1))+
+      ggplot(v, aes(x=seq(1:50))) + 
+        coord_cartesian(xlim = c(0, 50), ylim = c(0, 1))+
         geom_line(aes(y=act_immune_people, col="Immunität in der Bevölkerung"),lwd=2.5)+ 
         geom_line(aes(y=act_infect_people, col="Infektionen"),lwd=2.5)+
         geom_line(aes(y=act_dead_people, col="Todesfälle"),lwd=2.5)+
@@ -126,8 +126,8 @@ server <- function(input, output, session) {
     output$hospital <- renderPlot({
       #if (is.null(v$data)) return()
       #plot(v$act_immune_people)
-      ggplot(v, aes(x=seq(1:100))) + 
-        coord_cartesian(xlim = c(0, 100), ylim = c(0, 0.07))+
+      ggplot(v, aes(x=seq(1:50))) + 
+        coord_cartesian(xlim = c(0, 50), ylim = c(0, 0.07))+
         geom_line(aes(y=v$act_hosp_people, col="Anzahl Menschen die Platz in einem Krankenhaus haben"),lwd=2.5)+
         geom_line(aes(y=v$act_required_hosp, col="Anzahl Menschen die ins Krankenhaus müssen"),lwd=2.5)+
         theme_minimal()+
